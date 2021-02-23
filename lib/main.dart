ef4a450d1ac898a47de546473f76acf946c45d59
Jonjo McKay <jonjo@jonjomckay.com>
2021-02-23 23:44:38 +0000

    Made app theme color orange

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
@@ -33,18 +33,20 @@ class MyApp extends StatelessWidget {
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
+        primarySwatch: Colors.deepOrange,
+
+        // TODO: These are only required due to https://github.com/flutter/flutter/issues/19089
+        accentColor: Colors.deepOrange[500],
+        toggleableActiveColor: Colors.deepOrange[500],
+        textSelectionColor: Colors.deepOrange[200],
       ),
       themeMode: ThemeMode.system,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
